399fee2220e6a2f932607c0ddfb18da697ab2286
abdes000 <74131511+abdes000@users.noreply.github.com>
2022-07-03 06:22:10 -0700

    Update install_gui.sh

--- install_gui.sh
+++ install_gui.sh
@@ -1,7 +1,7 @@
 touch .hushlogin
 touch .vimrc
 sudo apt update && sudo apt upgrade && sudo apt-get update && sudo apt-get upgrade -y 
-sudo apt-get install libtool autoconf automake cmake libncurses5-dev g++ pacman cargo python3-dev python3-pip golang-go openjdk-17-jre-headless lua5.1 lua5.2 lua5.3 lua50 tarntool curl zsh tmux fish terminator neofetch dc ripgrep silversearcher-ag fdclone git xclip build-essential tasksel lolcat micro cargo &&
+sudo apt-get install libtool autoconf automake cmake libncurses5-dev g++ pacman cargo python3-dev python3-pip golang-go openjdk-17-jre-headless lua5.1 lua5.2 lua5.3 lua50 tarantool curl zsh tmux fish terminator neofetch dc ripgrep silversearcher-ag fdclone git xclip build-essential tasksel lolcat micro cargo &&
 sudo add-apt-repository ppa:neovim-ppa/unstable &&
 sudo apt-get update &&
 sudo apt-get install neovim &&
@@ -31,6 +31,7 @@ brew install gcc bat luajit tree-sitter lsd code-minimap wget starship &&
 source ~/.profile &&
 nvm install --lts &&
 git clone https://github.com/abdes000/Dotfiles.git &&
+git clone https://github.com/abdes000/nvimrc3.0.git &&
 mv Dotfiles/fish   ~/.config &&
 mv Dotfiles/htop   ~/.config &&
 mv Dotfiles/micro   ~/.config &&
@@ -40,6 +41,7 @@ mv Dotfiles/nvim   ~/.config &&
 mv Dotfiles/.bashrc   ~/ &&
 mv Dotfiles/.zshrc   ~/ &&
 mv Dotfiles/.tmux.conf   ~/ &&
+mv nvimrc3.0/  ~/.config &&
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
